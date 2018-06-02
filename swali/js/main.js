9f3bb8d025127f1fdb6eca486a91a5363eb279bf
mkulak <kleshney@gmail.com>
2018-06-02 20:20:40 +0200

    swali script PoC

--- swali/js/main.js
+++ swali/js/main.js
@@ -1,16 +1,23 @@
-document.onload = function() {
-    console.log("started")
-    alert("started");
-    callServer()
+window.onload = function(event) {
+    console.log("onload")
+    document.getElementById("submitButton").addEventListener("click", function() {
+        console.log("apiLink: " + apiLink)
+        var apiLink = document.getElementById("apiPathInput").value
+        callServer(apiLink)
+    })
 };
 
-function callServer() {
+function callServer(apiLink) {
+    console.log("apiLink: " + apiLink)
     var http = new XMLHttpRequest();
     var url = 'https://ijgf82g4o9.execute-api.us-west-2.amazonaws.com/api';
-    var body = '{"api_definition_url": "http://petstore.swagger.io/v2/swagger.json"}';
+    var body = '{"api_definition_url": "' + apiLink + '"}';
     http.open('POST', url, true);
     http.onreadystatechange = function() {
-        alert(http.responseText);
+        if (http.readyState === 4) {
+            // alert("" + http.status + " " + http.responseText);
+            document.getElementById("result").innerText = http.responseText
+        }
     }
     http.send(body)
 }
